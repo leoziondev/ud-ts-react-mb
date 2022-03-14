66dfb2d67364ec9b6a5bcf5038f9e972d14eb48d
LeoZion <leozionsuporte@gmail.com>
2022-03-14 02:48:09 -0300

    ts functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ function App() {
   const age: number = 32
   const isWorking: boolean = true
 
+  const userGreeting = (name: string): string => {
+    return `Bem vindo de volta ${name}!`
+  }
+
   return (
     <div className="App">
       <h1>React com Typescript</h1>
 
       <h2>Nome: {name}</h2>
+      <h3>{userGreeting(name)}</h3>
       <p>Idade: {age}</p>
       {isWorking && (
         <div><p>Yeeee, is working!</p></div>
-      )}
+      )}      
     </div>
   );
 }
