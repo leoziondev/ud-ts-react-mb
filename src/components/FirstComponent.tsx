fb963d433f69deefe9b5eb20aa9ae2caee1b1095
LeoZion <leozionsuporte@gmail.com>
2022-03-14 02:56:32 -0300

    create component

---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+const FirstComponent = (): ReactElement => {
+  return (
+    <div>
+        <h1>First Component</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
