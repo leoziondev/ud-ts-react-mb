34c78c62d60e42383cd8dfdfc0da662dedee6060
LeoZion <leozionsuporte@gmail.com>
2022-03-14 02:43:28 -0300

    variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function App() {
+  const name: string = "Jhon Doe"
+  const age: number = 32
+  const isWorking: boolean = true
+
   return (
     <div className="App">
       <h1>React com Typescript</h1>
+
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div><p>Yeeee, is working!</p></div>
+      )}
     </div>
   );
 }
